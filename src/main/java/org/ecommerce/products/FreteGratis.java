3f440f39ee9083480559d098d0334a5143e87716
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-11 20:21:44 -0300

    feat: extend PromocaoDecorator

--- src/main/java/org/ecommerce/products/FreteGratis.java
+++ src/main/java/org/ecommerce/products/FreteGratis.java
@@ -1,4 +1,13 @@
 package org.ecommerce.products;
 
-public class FreteGratis {
+public class FreteGratis extends PromocaoDecorator {
+    public FreteGratis(Produto produto) {
+        super(produto);
+    }
+
+    @Override
+    public double getPreco() {
+        return produto.getPreco();
+    }
+
 }
