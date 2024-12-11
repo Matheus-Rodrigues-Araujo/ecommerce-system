2440606e9ba2e8365d440f539705447baca78a11
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-11 20:20:51 -0300

    feat: create DescontoPercentual

--- src/main/java/org/ecommerce/products/DescontoPercentual.java
+++ src/main/java/org/ecommerce/products/DescontoPercentual.java
@@ -1,4 +1,15 @@
 package org.ecommerce.products;
 
-public class DescontoPercentual {
+public class DescontoPercentual extends PromocaoDecorator {
+    private double percentual;
+
+    public DescontoPercentual(Produto produto, double percentual) {
+        super(produto);
+        this.percentual = percentual;
+    }
+
+    @Override
+    public double getPreco() {
+        return produto.getPreco() * (1 - percentual / 100);
+    }
 }
