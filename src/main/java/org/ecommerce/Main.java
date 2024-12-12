a2942a6874f9696d94d8e37626db2a45ccbdf3c1
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-12 03:12:32 -0300

    feat: create a new product

--- src/main/java/org/ecommerce/Main.java
+++ src/main/java/org/ecommerce/Main.java
@@ -1,7 +1,15 @@
 package org.ecommerce;
 
+import org.ecommerce.products.DescontoPercentual;
+import org.ecommerce.products.FreteGratis;
+import org.ecommerce.products.Produto;
+import org.ecommerce.products.ProdutoBase;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Produto produto = new ProdutoBase("PS5", 3000.00);
+        produto = new DescontoPercentual(produto, 5);
+        produto = new FreteGratis(produto);
+        produto.getPreco();
     }
 }
