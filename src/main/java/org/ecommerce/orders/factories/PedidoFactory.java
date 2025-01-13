0c9e49973eb406acdbbc90d3d1aaffda32f30976
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-13 16:19:32 -0300

    chore: remove unnecessary classes

--- src/main/java/org/ecommerce/orders/factories/PedidoFactory.java
+++
@@ -1,7 +0,0 @@
-//package org.ecommerce.orders.factories;
-//
-//import org.ecommerce.orders.Pedido;
-//
-//public abstract class PedidoFactory {
-//    public abstract Pedido criarPedido(String descricao);
-//}
