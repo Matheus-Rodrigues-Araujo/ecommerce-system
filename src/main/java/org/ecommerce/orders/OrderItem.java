a681e1faf12cbf4cafd60424e873914b97713e8c
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-13 04:37:48 -0300

    feat: create OrderItem.java

---
+++ src/main/java/org/ecommerce/orders/OrderItem.java
@@ -0,0 +1,26 @@
+package org.ecommerce.orders;
+
+import org.ecommerce.products.IProduct;
+
+public class OrderItem {
+    private final IProduct product;
+    private final int quantity;
+
+    public OrderItem(IProduct product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public IProduct getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("OrderItem{product=%s, quantity=%d}", product, quantity);
+    }
+}
