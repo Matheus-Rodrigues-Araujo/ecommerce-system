a36de55c9cafb6ba325c4a2d4afd599093b63e98
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-13 04:42:00 -0300

    wip: developing Main.java logic

--- src/main/java/org/ecommerce/Main.java
+++ src/main/java/org/ecommerce/Main.java
@@ -1,64 +1,47 @@
 package org.ecommerce;
 
-import org.ecommerce.notifications.Cliente;
-import org.ecommerce.notifications.ProdutoNotifier;
-import org.ecommerce.orders.Pedido;
-import org.ecommerce.orders.TipoPedido;
-import org.ecommerce.orders.factories.PedidoFactoryManager;
-import org.ecommerce.products.DescontoPercentual;
-import org.ecommerce.products.Produto;
-import org.ecommerce.products.ProdutoBase;
-import org.ecommerce.shipping.CalculadorFrete;
-import org.ecommerce.shipping.FretePorDistanciaStrategy;
-import org.ecommerce.shipping.FretePorPesoStrategy;
+import org.ecommerce.clients.Client;
+import org.ecommerce.orders.Order;
+import org.ecommerce.payments.CreditCard;
+import org.ecommerce.payments.IPayment;
+import org.ecommerce.payments.PicPay;
+import org.ecommerce.payments.strategies.CreditCardPayment;
+import org.ecommerce.payments.strategies.PicPayPayment;
+import org.ecommerce.products.DiscountedProduct;
+import org.ecommerce.products.IProduct;
+import org.ecommerce.products.Product;
 
 public class Main {
+    private static final Order order = new Order();
+    private static IPayment payment;
+    private static final int paymentMethod = 1;
+
     public static void main(String[] args) {
-        System.out.println("--------- DECORATOR --------");
-        Produto produto = new ProdutoBase("PS5", 3000.00);
-        produto = new DescontoPercentual(produto, 5);
-        System.out.println("C/Desconto: " + produto.getPreco());
 
-        System.out.println();
+        Client client = new Client("John", "Doe", "johndoe@gmail.com", "87654321");
 
-        System.out.println("--------- STRATEGY ---------");
-        CalculadorFrete calculadorFrete = new CalculadorFrete();
-        calculadorFrete.setEstrategiaFrete(new FretePorPesoStrategy());
-        calculadorFrete.setEstrategiaFrete(new FretePorDistanciaStrategy());
-        System.out.println("Custo de frete: " + calculadorFrete.calcular(100,2));
+        //Decorator
+        IProduct product1 = new Product("Ps5", 4500.00);
+        IProduct product2 = new DiscountedProduct(new Product("Berserk Vol.1", 120.00), 0.15);
 
-        System.out.println();
+        order.addItem(product1, 1);
+        order.addItem(product2, 2);
+        System.out.println(order.getItems());
 
-        System.out.println("--------- FACTORY ----------");
-        // Pedido Padrão
-        Pedido pedidoPadrao = PedidoFactoryManager.criarPedido(TipoPedido.PADRAO, "TV 42 polegadas");
-        pedidoPadrao.processar();
+        // Strategy
+        CreditCard creditCard = new CreditCard("1234", "09/10/2099", "000");
+        PicPay picPay = new PicPay(client.getEmail(), "crazyHash123!");
 
-        System.out.println("----------------------------");
+        switch (paymentMethod) {
+            case 1:
+                payment = new PicPayPayment();
+                break;
+            case 2:
+                payment = new CreditCardPayment();
+                break;
+        }
 
-        // Pedido Expresso
-        Pedido pedidoExpresso = PedidoFactoryManager.criarPedido(TipoPedido.EXPRESSO, "Notebook Gamer");
-        pedidoExpresso.processar();
-
-        System.out.println();
-
-        System.out.println("--------- OBSERVER ---------");
-
-        ProdutoNotifier notifier = new ProdutoNotifier();
-
-        Cliente cliente1 = new Cliente("Brksedu");
-        Cliente cliente2 = new Cliente("Davi Jones");
-        Cliente cliente3 = new Cliente("GameplayRJ");
-        Cliente cliente4 = new Cliente("Michael Jackson");
-        Cliente cliente5 = new Cliente("Kratos");
-
-        notifier.adicionarObservador(cliente1);
-        notifier.adicionarObservador(cliente2);
-        notifier.adicionarObservador(cliente3);
-        notifier.adicionarObservador(cliente4);
-        notifier.adicionarObservador(cliente5);
-
-        notifier.notificar("Novo produto disponível");
+        order.proccessOrder(payment);
 
     }
 }
