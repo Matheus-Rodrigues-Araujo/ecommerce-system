e991ebb4046007e7a2dd8e8f0862a6f2d42d6125
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-13 16:26:05 -0300

    chore: remove unused files

--- src/main/java/org/ecommerce/products/factories/ProdutoFactory.java
+++
@@ -1,7 +0,0 @@
-package org.ecommerce.products.factories;
-
-import org.ecommerce.products.Produto;
-
-public abstract class ProdutoFactory {
-    public abstract Produto criarProduto(String nome, double preco);
-}
