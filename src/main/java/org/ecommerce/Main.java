1fdd05d4651f65cc1171a0cc30946295854f440d
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-13 16:24:17 -0300

    refactor: refactor organization and logic

--- src/main/java/org/ecommerce/Main.java
+++ src/main/java/org/ecommerce/Main.java
@@ -1,7 +1,8 @@
 package org.ecommerce;
 
 import org.ecommerce.clients.Client;
-import org.ecommerce.orders.Order;
+import org.ecommerce.orders.factories.OrderFactory;
+import org.ecommerce.orders.models.IOrder;
 import org.ecommerce.payments.CreditCard;
 import org.ecommerce.payments.IPayment;
 import org.ecommerce.payments.PicPay;
@@ -12,7 +13,6 @@ import org.ecommerce.products.IProduct;
 import org.ecommerce.products.Product;
 
 public class Main {
-    private static final Order order = new Order();
     private static IPayment payment;
     private static final int paymentMethod = 1;
 
@@ -20,13 +20,20 @@ public class Main {
 
         Client client = new Client("John", "Doe", "johndoe@gmail.com", "87654321");
 
-        //Decorator
-        IProduct product1 = new Product("Ps5", 4500.00);
-        IProduct product2 = new DiscountedProduct(new Product("Berserk Vol.1", 120.00), 0.15);
+        // Decorator
+        IProduct product1 = new Product("Mesa Pro Gamer", 2950.00);
+        IProduct product2 = new DiscountedProduct(new Product("Berserk Vol.1", 90.00), 0.15);
+        System.out.printf("> %s added new item: %s%n", client.getFirstName(), product1);
+        System.out.printf("> %s added new item: %s%n", client.getFirstName(), product2);
 
+        // Factory
+        System.out.println("------------------------");
+        IOrder order = OrderFactory.createOrder("express");
         order.addItem(product1, 1);
         order.addItem(product2, 2);
-        System.out.println(order.getItems());
+        System.out.printf("> Delivery Type: %s%n", order.getDeliveryType());
+        System.out.printf("> Items in the Order: %s%n", order.getItems());
+        System.out.printf("> Total Order Amount: %s%n", order.calculateTotal());
 
         // Strategy
         CreditCard creditCard = new CreditCard("1234", "09/10/2099", "000");
@@ -34,14 +41,13 @@ public class Main {
 
         switch (paymentMethod) {
             case 1:
-                payment = new PicPayPayment();
+                payment = new PicPayPayment(picPay);
                 break;
             case 2:
-                payment = new CreditCardPayment();
+                payment = new CreditCardPayment(creditCard);
                 break;
         }
 
         order.proccessOrder(payment);
-
     }
 }
