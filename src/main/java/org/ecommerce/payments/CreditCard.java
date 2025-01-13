fc0f5820108cc376b898d81ed235613d9bb81417
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-13 16:23:16 -0300

    feat: add verify method

--- src/main/java/org/ecommerce/payments/CreditCard.java
+++ src/main/java/org/ecommerce/payments/CreditCard.java
@@ -2,23 +2,27 @@ package org.ecommerce.payments;
 
 public class CreditCard {
 
-    private int amount;
-    private String number;
-    private String date;
-    private String cvv;
+    private double amount;
+    private final String number;
+    private final String date;
+    private final String cvv;
 
     public CreditCard(String number, String date, String cvv) {
-        this.amount = 1500;
+        this.amount = 5500;
         this.number = number;
         this.date = date;
         this.cvv = cvv;
     }
 
-    public void setAmount(int amount) {
+    public boolean verify(String inputNumber, String inputDate, String inputCvv) {
+        return number.equals(inputNumber) && date.equals(inputDate) && cvv.equals(inputCvv);
+    }
+
+    public void setAmount(double amount) {
         this.amount = amount;
     }
 
-    public int getAmount() {
+    public double getAmount() {
         return amount;
     }
 
