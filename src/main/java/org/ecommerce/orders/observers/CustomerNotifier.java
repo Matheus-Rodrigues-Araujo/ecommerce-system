8a4f7de03f0049b70b75ee56be05b0e941e6e4fc
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-26 01:07:14 -0300

    feat: create CustomerNotifier class

---
+++ src/main/java/org/ecommerce/orders/observers/CustomerNotifier.java
@@ -0,0 +1,8 @@
+package org.ecommerce.orders.observers;
+
+public class CustomerNotifier implements IObserver {
+    @Override
+    public void update(String event, String message) {
+        System.out.printf("NOTIFICATION [%s]: %s%n", event, message);
+    }
+}
